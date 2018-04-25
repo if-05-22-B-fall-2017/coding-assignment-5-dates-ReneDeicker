660f10497401e609423543004a1b1158a791ece2
ReneDeicker <rene.deicker1105@gmail.com>
2018-04-25 23:52:05 +0200

    unittests solved

--- src/main/java/GermanStyleDateChecker.java
+++ src/main/java/GermanStyleDateChecker.java
@@ -3,6 +3,8 @@ import java.util.regex.Pattern;
 
 public class GermanStyleDateChecker {
     public static Matcher getMatcher(String dateString) {
-        return null;
+        Pattern p = Pattern.compile("^([0-2]?\\d|3(0|1))[- .]((\\d(0|1|2))|(0\\d)|(\\d))[- .]((19)|(20))?\\d\\d$");
+        Matcher m = p.matcher(dateString);
+        return m;
     }
 }
